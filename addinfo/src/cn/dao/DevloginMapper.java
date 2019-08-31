35e8c32617af6d53de03f173f4f8586b70604afc
t240lige <1802279068@qq.com>
2019-08-31 11:55:23 +0800

    jsp和statics好了

--- addinfo/src/cn/dao/DevloginMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.dao;
-
-import org.apache.ibatis.annotations.Param;
-
-import cn.entity.Dev_User;
-
-public interface DevloginMapper {
-	public Dev_User devLogin(@Param("devCode")String devCode,@Param("devpassword")String devpassword);
-}
